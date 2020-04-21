b4f3766665902dc7976b4c9924f9e6a49f7bfe03
Juston Hart <contact@justonh.art>
2020-04-21 14:50:38 -0500

    Implement required features for project

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,43 @@
 
-addGridItems();
+initPage();
 
-function addGridItems(){
+function buildGrid(rowlength = 16){
     const parent = document.getElementById("gridContainer");
 
     while(parent.children.length) parent.removeChild(parent.children[0]);
+
+    let div = document.createElement("DIV");
+    div.style.backgroundColor = "white";
+    div.style.width = (100/rowlength) + "%";
+    div.style.height = (100/rowlength) + "%";
+
+    
+    for(let i = 0; i < rowlength**2;  i++){
+        let clone = div.cloneNode();
+        clone.addEventListener('mouseover', function(){this.style.backgroundColor = "black";});
+        parent.appendChild(clone);
+    } 
+}
+
+function initPage(){
+    buildGrid();
+    document.getElementById("resetButton").addEventListener("click", buttonPrompt);
+    document.getElementById("clearButton").addEventListener("click", clearGrid);
+}
+
+function buttonPrompt(){
+    let input = prompt("enter a number between 1 and 100");
+
+    if(!input)
+        return;
+        
+    if(input < 1 || input > 100)
+        return buttonPrompt();
+    
+    buildGrid(input);
+}
+
+function clearGrid(){
+    const gridSize = Math.sqrt(document.getElementById("gridContainer").children.length);
+    buildGrid(gridSize);
 }
